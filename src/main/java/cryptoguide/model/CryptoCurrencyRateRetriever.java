42cb97ceb16e45921e416e5051526de4746e77b0
devtobi <tobias.stadlerstadler@gmx.de>
2018-12-03 19:01:12 +0100

    Added a new method to the CryptoCurrencyRateRetriever for retrieving past rates via a timestamp. =========================================== Wrote tests for the added method

--- src/main/java/cryptoguide/model/CryptoCurrencyRateRetriever.java
+++ src/main/java/cryptoguide/model/CryptoCurrencyRateRetriever.java
@@ -21,9 +21,11 @@ public class CryptoCurrencyRateRetriever {
     }
 
     private static final String CURRENT_RATE_REQUEST = "https://min-api.cryptocompare.com/data/price?";
+    private static final String PAST_RATE_REQUEST = "https://min-api.cryptocompare.com/data/pricehistorical?";
     private static final Logger logger = Logger.getLogger(CryptoCurrencyRateRetriever.class.getName());
 
     public static double getCurrentRate(String primaryCurrency, String secondaryCurrency) {
+        if(primaryCurrency.equals(secondaryCurrency)) return 1.00;
         double value = 0.0;
         try {
             HttpsURLConnection getRateConnection = (HttpsURLConnection) new URL(CURRENT_RATE_REQUEST + "fsym=" + primaryCurrency + "&tsyms=" + secondaryCurrency).openConnection();
@@ -39,6 +41,27 @@ public class CryptoCurrencyRateRetriever {
         }
 
         return value;
+    }
+
+    public static double getPastRate(String primaryCurrency, String secondaryCurrency, long timestamp) {
+        if(primaryCurrency.equals(secondaryCurrency)) return 1.00;
+        if(timestamp >= TimestampGenerator.getCurrentTimeStamp()) {
+            return getCurrentRate(primaryCurrency, secondaryCurrency);
+        } else {
+            double value = 0.0;
+            try {
+                HttpsURLConnection getRateConnection = (HttpsURLConnection) new URL(PAST_RATE_REQUEST + "fsym=" + primaryCurrency + "&tsyms=" + secondaryCurrency + "&ts=" + timestamp).openConnection();
+                getRateConnection.setRequestMethod("GET");
+                InputStreamReader rateReader = new InputStreamReader(getRateConnection.getInputStream());
+                JsonObject jsonResponse = Json.parse(rateReader).asObject();
+                if (jsonResponse.get(primaryCurrency) != null && jsonResponse.get(primaryCurrency).asObject().get(secondaryCurrency) != null) {
+                    value = jsonResponse.get(primaryCurrency).asObject().get(secondaryCurrency).asDouble();
+                }
+            } catch (IOException e) {
+                logger.log(Level.WARNING, "Could not connect to the CryptoCompare-API or created a IOException during connection");
+            }
+            return value;
+        }
     }
 
 }
---
+++ src/test/java/cryptoguide/model/CryptoCurrencyRateRetrieverTest.java
@@ -0,0 +1,111 @@
+package cryptoguide.model;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CryptoCurrencyRateRetrieverTest {
+
+    @Test
+    public void GetCurrencyRateTest1() {
+        String firstCurrency = "BTC";
+        String secondCurrency = "USD";
+        double test = CryptoCurrencyRateRetriever.getCurrentRate(firstCurrency, secondCurrency);
+        Assert.assertTrue(test != 0.0f);
+    }
+
+    @Test
+    public void GetCurrencyRateTest2() {
+        String firstCurrency = "EUR";
+        String secondCurrency = "ETH";
+        double test = CryptoCurrencyRateRetriever.getCurrentRate(firstCurrency, secondCurrency);
+        Assert.assertNotEquals(test,0.0f);
+    }
+
+    @Test
+    public void GetCurrencyRateTest3() {
+        String firstCurrency = "LTC";
+        String secondCurrency = "BTC";
+        double test = CryptoCurrencyRateRetriever.getCurrentRate(firstCurrency, secondCurrency);
+        Assert.assertNotEquals(test,0.0f);
+    }
+
+    @Test
+    public void GetCurrencyRateTest4() {
+        String firstCurrency = "BTC";
+        String secondCurrency = "BTC";
+        double test = CryptoCurrencyRateRetriever.getCurrentRate(firstCurrency, secondCurrency);
+        Assert.assertEquals(1.0f, test,0.0f);
+    }
+
+    @Test
+    public void GetCurrencyRateTest5() {
+        String firstCurrency = "sadboi";
+        String secondCurrency = "BTC";
+        double test = CryptoCurrencyRateRetriever.getCurrentRate(firstCurrency, secondCurrency);
+        Assert.assertEquals(0.0f, test,0.0f);
+    }
+
+    @Test
+    public void GetCurrencyRateTest6() {
+        String firstCurrency = "BTC";
+        String secondCurrency = "asdfasdf";
+        double test = CryptoCurrencyRateRetriever.getCurrentRate(firstCurrency, secondCurrency);
+        Assert.assertEquals(0.0f, test, 0.0f);
+    }
+
+    @Test
+    public void GetCurrencyRateTest7() {
+        String firstCurrency = "errupwerüü";
+        String secondCurrency = "asdfasdf";
+        double test = CryptoCurrencyRateRetriever.getCurrentRate(firstCurrency, secondCurrency);
+        Assert.assertEquals(0.0f, test, 0.0f);
+    }
+
+    @Test
+    public void GetPastRateTestInvalid() {
+        String firstCurrency = "notACurrency";
+        String secondCurrency = "BTC";
+        long timestamp = TimestampGenerator.convertToTimeStamp("2016-12-09");
+        double test = CryptoCurrencyRateRetriever.getPastRate(firstCurrency, secondCurrency, timestamp);
+        Assert.assertEquals(0.0f, test, 0.0f);
+    }
+
+    @Test
+    public void GetPastRateTestFuture() {
+        String firstCurrency = "BTC";
+        String secondCurrency = "EUR";
+        long timestamp = TimestampGenerator.convertToTimeStamp("2999-12-09");
+        double test = CryptoCurrencyRateRetriever.getPastRate(firstCurrency, secondCurrency, timestamp);
+        double test_actual = CryptoCurrencyRateRetriever.getCurrentRate(firstCurrency, secondCurrency);
+        Assert.assertEquals(test_actual, test, 0);
+    }
+
+    @Test
+    public void GetPastRateTestPast() {
+        String firstCurrency = "BTC";
+        String secondCurrency = "EUR";
+        long timestamp = TimestampGenerator.convertToTimeStamp("2016-12-09");
+        double expected = 734.11;
+        double actual = CryptoCurrencyRateRetriever.getPastRate(firstCurrency, secondCurrency, timestamp);
+        Assert.assertEquals(expected, actual, 0);
+    }
+
+    @Test
+    public void GetPastRateTestFarPast() {
+        String firstCurrency = "BTC";
+        String secondCurrency = "EUR";
+        long timestamp = TimestampGenerator.convertToTimeStamp("1970-12-09");
+        double expected = 0.00;
+        double actual = CryptoCurrencyRateRetriever.getPastRate(firstCurrency, secondCurrency, timestamp);
+        Assert.assertEquals(expected, actual, 0);
+    }
+
+    @Test
+    public void GetPastRateTestSameCurrency() {
+        String firstCurrency = "USD";
+        String secondCurrency = "USD";
+        long timestamp = TimestampGenerator.convertToTimeStamp("2013-11-18");
+        double actual = CryptoCurrencyRateRetriever.getPastRate(firstCurrency, secondCurrency, timestamp);
+        Assert.assertEquals(1, actual, 0);
+    }
+}
