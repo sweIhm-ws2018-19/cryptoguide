d4e9cb131aee96b3a41a1908f6da97336a95bf58
devtobi <tobias.stadlerstadler@gmx.de>
2018-12-04 17:31:42 +0100

    Fixed some code smells

--- src/main/java/cryptoguide/model/TimestampGenerator.java
+++ src/main/java/cryptoguide/model/TimestampGenerator.java
@@ -22,21 +22,19 @@ public class TimestampGenerator {
         int inputLength = input.length();
         if((inputLength == 8 || inputLength == 7) && input.charAt(5) == 'W') {
             //Calculate the date of monday in this specific week
-            int year = Integer.valueOf(input.substring(0,4));
-            int week = Integer.valueOf(input.substring(6,inputLength));
+            int year = Integer.parseInt(input.substring(0,4));
+            int week = Integer.parseInt(input.substring(6,inputLength));
             LocalDate date = LocalDate.ofYearDay(year, 20).with(IsoFields.WEEK_OF_WEEK_BASED_YEAR, week).with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY));
             input = date.toString();
         } else if((inputLength == 11 || inputLength == 10) && input.endsWith("WE")) {
             //Calculate the date of sunday in this specific week
-            int year = Integer.valueOf(input.substring(0,4));
-            int week = Integer.valueOf(input.substring(6,inputLength - 3));
+            int year = Integer.parseInt(input.substring(0,4));
+            int week = Integer.parseInt(input.substring(6,inputLength - 3));
             LocalDate date = LocalDate.ofYearDay(year, 20).with(IsoFields.WEEK_OF_WEEK_BASED_YEAR, week).with(TemporalAdjusters.nextOrSame(DayOfWeek.SUNDAY));
             input = date.toString();
-        } else if(inputLength == 10) {
-            if (input.endsWith("XX")) {
-                //Calculate the date of the first
-                input = input.replace("XX", "01");
-            }
+        } else if(inputLength == 10 && input.endsWith("XX")) {
+            //Calculate the date of the first
+            input = input.replace("XX", "01");
         }
         return getTimeStampForDateString(input);
     }
@@ -44,9 +42,9 @@ public class TimestampGenerator {
     static long getTimeStampForDateString(String dateString) {
         if(dateString.length() == 10) {
             //dateString has schema 2016-04-17
-            int year = Integer.valueOf(dateString.substring(0,4));
-            int month = Integer.valueOf(dateString.substring(5,7));
-            int day = Integer.valueOf(dateString.substring(8, 10));
+            int year = Integer.parseInt(dateString.substring(0,4));
+            int month = Integer.parseInt(dateString.substring(5,7));
+            int day = Integer.parseInt(dateString.substring(8, 10));
             LocalDate date = LocalDate.of(year, month, day);
             return date.atStartOfDay().toInstant(ZoneOffset.UTC).getEpochSecond();
         }
