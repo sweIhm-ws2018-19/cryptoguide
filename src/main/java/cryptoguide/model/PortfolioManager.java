6511b0b4f2c540fa0695c39022c50691ef3d3f1f
devtobi <tobias.stadlerstadler@gmx.de>
2018-12-20 13:04:59 +0100

    Added PortfolioManager

---
+++ src/main/java/cryptoguide/model/PortfolioManager.java
@@ -0,0 +1,66 @@
+package cryptoguide.model;
+
+import com.amazon.ask.attributes.AttributesManager;
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+
+import java.util.Map;
+
+public class PortfolioManager {
+
+    // This is a static class so we need a protected constructor to hide the standard public one.
+    PortfolioManager() {
+        throw new IllegalStateException("This is a static class, can't create a instance.");
+    }
+
+    public static Map<String, Object> getPortfolioContent(HandlerInput input) {
+        return input.getAttributesManager().getPersistentAttributes();
+    }
+
+    public static int getAmount(HandlerInput input, String currencyCode) {
+        Map<String, Object> all = getPortfolioContent(input);
+        if(!all.containsKey(currencyCode)) {
+            return 0;
+        } else {
+            Object obj = all.get(currencyCode);
+            return (int) obj;
+        }
+    }
+
+    public static boolean addAmount(HandlerInput input, String currencyCode, int amount) {
+        if(amount <= 0) {
+            return false;
+        } else {
+            int amountInDB = getAmount(input, currencyCode);
+            AttributesManager manager = input.getAttributesManager();
+            Map<String, Object> attributes = manager.getPersistentAttributes();
+            attributes.put(currencyCode, amountInDB + amount);
+            manager.setPersistentAttributes(attributes);
+            manager.savePersistentAttributes();
+            return true;
+        }
+    }
+
+    public static boolean removeAmount(HandlerInput input, String currencyCode, int amount) {
+        if (amount <= 0) {
+            return false;
+        } else {
+            int amountInDB = getAmount(input, currencyCode);
+            int newValue = amountInDB - amount;
+            if (newValue < 0) {
+                return false;
+            } else {
+                AttributesManager manager = input.getAttributesManager();
+                Map<String, Object> attributes = manager.getPersistentAttributes();
+                if (newValue == 0) {
+                    attributes.remove(currencyCode);
+                } else {
+                    attributes.put(currencyCode, newValue);
+                }
+                manager.setPersistentAttributes(attributes);
+                manager.savePersistentAttributes();
+                return true;
+            }
+        }
+    }
+
+}
